b9857eb7f5d9bf84f8e24d38d1103b38a7667727
Dominic Petriella <dompetriella@gmail.com>
2025-08-30 12:42:13 -0400

    Made audio assignable

--- scripts/base/AudioManager.cs
+++ scripts/base/AudioManager.cs
@@ -5,39 +5,39 @@ using System.Threading.Tasks;
 public partial class AudioManager : Node
 {
     public static AudioManager Instance { get; private set; }
-    private AudioStreamPlayer Music;
+    
+    [Export]
+    private AudioStreamPlayer MusicNode;
 
 
     public override void _Ready()
     {
         base._Ready();
         Instance = this;
-
-        Music = GetNode<AudioStreamPlayer>("%Music");
     }
 
     public void PlayMusic(AudioStream audioStream, float fadeInTime = 0.0f)
     {
-        if (Music.Playing)
+        if (MusicNode.Playing)
         {
-            Music.Stop();
+            MusicNode.Stop();
 
-            Music.VolumeDb = 0;
+            MusicNode.VolumeDb = 0;
         }
 
-        Music.Stream = audioStream;
+        MusicNode.Stream = audioStream;
 
         if (fadeInTime > 0.0f)
         {
-            Music.VolumeDb = -80;
-            Music.Play();
+            MusicNode.VolumeDb = -80;
+            MusicNode.Play();
             Tween musicTween = GetTree().CreateTween();
-            musicTween.TweenProperty(Music, AudioStreamPlayerProperties.VolumeDb, 0, fadeInTime);
+            musicTween.TweenProperty(MusicNode, AudioStreamPlayerProperties.VolumeDb, 0, fadeInTime);
         }
         else
         {
-            Music.VolumeDb = 0;
-            Music.Play();
+            MusicNode.VolumeDb = 0;
+            MusicNode.Play();
         }
 
     }
